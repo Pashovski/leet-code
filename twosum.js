aca60183aa5e2c5b84ba173f91c233d809782853
Pashovski <pashdoctor@gmail.com>
2022-03-22 11:37:02 -0400

    added solution for twosum

--- twosum.js
+++ twosum.js
@@ -5,4 +5,27 @@
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-//Git Identity Test Line
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  // loop over nums array
+  // for each element in nums, loop over nums again
+  // check if current element + inner loop element === target
+  // if true, return array with index of current element and
+  // index of inner loop element
+  // make sure not to use same element twice
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i === j) {
+        continue
+      } else if (nums[i] + nums[j] === target) {
+        return [i, j]
+      }
+    }
+  }
+}
+
+console.log(twoSum([0, 1, 2, 6, 5, 7], 9))
